be3b7a60693e02be3a8567d990512824b94fe909
Thomas Jalabert <thom.jalabert@gmail.com>
2017-12-08 12:04:55 +0100

    Add class TreeClassifier, add launcher, add manual learning

---
+++ LOOV/tree_classifiers/tree_launcher.py
@@ -0,0 +1,52 @@
+from keras.preprocessing.image import ImageDataGenerator
+from TreeClassifier import TreeClassifier
+from keras.engine import Model
+from keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
+from keras.models import Sequential, save_model, load_model
+from keras.layers import Dropout, Flatten, Dense, MaxPooling2D, Conv2D
+from keras import applications, Input, losses, metrics
+from keras.optimizers import Adam
+import numpy as np
+import os
+import json
+from PIL import Image
+
+separator = os.sep
+
+datagen = ImageDataGenerator(
+    featurewise_center=False,
+    samplewise_center=False,
+    featurewise_std_normalization=False,
+    samplewise_std_normalization=False,
+    # zca_whitening=True,
+    # zca_epsilon=1e-6,
+    rotation_range=20,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    shear_range=0.1,
+    zoom_range=0.2,
+    # channel_shift_range=0.,
+    fill_mode='nearest',
+    cval=0.,
+    horizontal_flip=True,
+    vertical_flip=True,
+    preprocessing_function=None  # On pourrai peut être resize ici
+    # data_format=K.image_data_format()
+)
+
+super_path = "E:\\Polytech_Projects\\pfe_plankton_classif\\LOOV\\super_classif"
+# super_path = "E:\\Polytech_Projects\\pfe_plankton_classif\Dataset\\PFE"
+classifier = TreeClassifier(super_path)
+
+# classifier.create_achitecture(datagen, nb_epoch=10)
+
+# path_classif = "E:\\Polytech_Projects\\pfe_plankton_classif\\LOOV\\super_classif\\living\\meduses\\Hydrozoa"
+# path_classif = "E:\\Polytech_Projects\\pfe_plankton_classif\\LOOV\\super_classif\\fiber"
+path_classif = "E:\\Polytech_Projects\\pfe_plankton_classif\\LOOV\\super_classif\\living\\Copecope\\Copepoda"
+
+# path_classif = "E:\Polytech_Projects\pfe_plankton_classif\LOOV\super_classif\living\globe_avec_points\Rhizaria"
+
+# classifier.classify(path_classif)
+
+
+classifier.create_manual(nb_epoch=3, nb_batch=500)
