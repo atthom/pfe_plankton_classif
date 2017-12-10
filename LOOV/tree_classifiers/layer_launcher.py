2bed94036e674c9f8d5730bee791839d0a54d9b5
Thomas Jalabert <thom.jalabert@gmail.com>
2017-12-10 15:39:55 +0100

    Layer Classifier can fit manually

---
+++ LOOV/tree_classifiers/layer_launcher.py
@@ -0,0 +1,9 @@
+import os
+from LayerClassifier import LayerClassifier
+
+super_path = "E:\\Polytech_Projects\\pfe_plankton_classif\\LOOV\\super_classif"
+
+lc = LayerClassifier(super_path)
+
+
+lc.create_achitecture(500, 3)
