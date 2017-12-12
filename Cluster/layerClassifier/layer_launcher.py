cb5905dc3a71c4877759bdf63d3e33b7abd28898
Thomas Jalabert <thom.jalabert@gmail.com>
2017-12-12 13:08:19 +0100

    modif for cluster
    Former-commit-id: 6a081443bc4f5a3a501d6bcddaa76cd11ed01b9b [formerly b8d61eed7cd7d6f936b825c3fd301668b526c631]
    Former-commit-id: cf76270e6c6e26445ee88854b3ea8b6f39fb8e08

--- Cluster/layerClassifier/layer_launcher.py
+++ Cluster/layerClassifier/layer_launcher.py
@@ -3,8 +3,9 @@ from LayerClassifier import LayerClassifier
 
 super_path = "E:\\Polytech_Projects\\pfe_plankton_classif\\LOOV\\super_classif"
 super_path = "E:\\Polytech_Projects\\pfe_plankton_classif\\Dataset\\DATASET\\level0_new_hierarchique"
+super_path = "/home/tjalaber/pfe_plankton_classif/Cluster/Classif2"
 
 lc = LayerClassifier(super_path)
-lc.create_achitecture(500, 5)
+lc.create_achitecture(1000, 5)
 
 # lc.load_achitecture()
