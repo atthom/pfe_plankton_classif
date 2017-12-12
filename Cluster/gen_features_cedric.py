d6a4e537bf7b5596d37e8a9f361511c206361645
Thomas Jalabert <thom.jalabert@gmail.com>
2017-12-12 11:19:20 +0100

    modif for cluster

--- Cluster/gen_features_cedric.py
+++ Cluster/gen_features_cedric.py
@@ -28,23 +28,31 @@ train_data_dir = "E:\\Polytech_Projects\\pfe_plankton_classif\\Dataset\\train"
 
 def create_model():
     model = Sequential()
-    model.add(Conv2D(32, (3, 3), activation='relu',
-                     input_shape=(150, 150, 1)))
+
+    model.add(Conv2D(32, (3, 3), padding='same',
+                     input_shape=(150, 150, 1),
+                     activation='relu'))
     model.add(Conv2D(32, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
+    model.add(Dropout(0.2))
 
-    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(Conv2D(64, (3, 3), padding='same',
+                     activation='relu'))
     model.add(Conv2D(64, (3, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
 
-    model.add(Flatten())
-    model.add(Dense(256, activation='relu'))
-    model.add(Dense(nb_classes, activation='softmax'))
+    model.add(Conv2D(128, (3, 3), padding='same',
+                     activation='relu'))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.2))
 
-    adam = Adam(lr=0.000001)
-    model.compile(loss=losses.categorical_crossentropy,
-                  optimizer=adam, metrics=[metrics.categorical_accuracy])
+    model.add(Conv2D(256, (3, 3), padding='same',
+                     activation='relu'))
+    model.add(Conv2D(256, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
     return model
 
 
